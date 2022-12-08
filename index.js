87fa593878bfaa3e4a05663a92f31f968d128783
praiseolotu <olioxxayo@gmail.com>
2022-12-08 20:13:53 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ console.log(a, b);
 // work with arrays
 food = ["beans", "rice", "yam"];
 console.log(`I love to eat ${food[1]} in the morning.`);
+
+for (let i = 0; i < food.length; i++) {
+  console.log(food[i]);
+}
